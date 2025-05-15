76bc339d5137c146ada7a213293575a2f499f741
smplote <smplote@gmail.com>
2025-05-15 22:59:15 +0800

    gitee 回调数据修复

--- service/gitee/gitee_webhook_service.py
+++ service/gitee/gitee_webhook_service.py
@@ -40,13 +40,13 @@ class GiteeWebhookService(Que):
         db = CallbackQueueModel()
         db.set_processed(pid)
         if status == 200 and 'markdown' in data:
+            # 发送企业应用消息
+            res['send_msg'] = QyClient().send_msg(data['markdown'])
             if Config.is_prod():
                 # 延迟拉取最新代码并重启 flask
                 def pull_code():
                     Sys.run_command('/opt/shell/init/reload_flask.sh')
                 res['git_pull'] = Sys.delayed_task(3, pull_code)
-            # 发送企业应用消息
-            res['send_msg'] = QyClient().send_msg(data['markdown'])
             # 更新处理结果
             update_data = {"process_result": res}
             if res['send_msg']:
