5418a3a5618ad6f87bf2b0b05f593ed8d3f1d294
smplote <smplote@gmail.com>
2025-08-13 17:41:46 +0800

    日志修复

--- tool/unit/gpl/em_data_source.py
+++ tool/unit/gpl/em_data_source.py
@@ -85,7 +85,7 @@ class EmDataSource:
             except Exception as e:
                 err = Error.handle_exception_info(e)
                 logger.warning(f"请求失败 ({i + 1}/{self.retry_times}): {url} - {params} - 错误 - {err}", 'EM_API_ERR')
-                self.ldb.update_gpl_api_log(pid, {'response_result': str(err), 'request_params': params})
+                # self.ldb.update_gpl_api_log(pid, {'response_result': str(err), 'request_params': params})
                 if i == self.retry_times - 1:
                     return None, 0
 
