474d37548aa1a42ad7739063c1879cdc18bda8af
smplote <smplote@gmail.com>
2025-10-27 09:28:14 +0800

    指令调整

--- app/bot/task.py
+++ app/bot/task.py
@@ -29,9 +29,9 @@ class Task(BaseAppVp):
         for g_wxid in g_list:
             res[g_wxid] = {}
             res[g_wxid]['vp_sky_hs'] = Sys.delayed_task(sky_task_exec, g_wxid, s_wxid, 'vp_sky_hs', '', 1, delay_seconds=0.1 + i)
-            res[g_wxid]['vp_sky_rw'] = Sys.delayed_task(sky_task_exec, g_wxid, s_wxid, 'vp_sky_rw', '', 1, delay_seconds=30 + i) # > 20
-            res[g_wxid]['vp_xw'] = Sys.delayed_task(sky_task_exec, g_wxid, s_wxid, 'vp_xw', delay_seconds=90 + i)  # > 50
-            res[g_wxid]['vp_ov_wa'] = Sys.delayed_task(sky_task_exec, g_wxid, s_wxid, 'vp_ov_wa', delay_seconds=150 + i)  # > 30
+            res[g_wxid]['vp_sky_rw'] = Sys.delayed_task(sky_task_exec, g_wxid, s_wxid, 'vp_sky_rw', '', 1, delay_seconds=20 + i) # > 15
+            res[g_wxid]['vp_xw'] = Sys.delayed_task(sky_task_exec, g_wxid, s_wxid, 'vp_xw', delay_seconds=100 + i)  # > 50
+            res[g_wxid]['vp_ov_wa'] = Sys.delayed_task(sky_task_exec, g_wxid, s_wxid, 'vp_ov_wa', delay_seconds=160 + i)  # > 30
             i += 120
         return self.success(res)
 
