2fed2d4f453dc4207dd5bee3c9d109a74fd4fc6e
smplote <smplote@gmail.com>
2025-10-29 19:12:27 +0800

    指令调整

--- app/bot/task.py
+++ app/bot/task.py
@@ -32,7 +32,7 @@ class Task(BaseAppVp):
             res[g_wxid]['vp_sky_rw'] = Sys.delayed_task(sky_task_exec, g_wxid, s_wxid, 'vp_sky_rw', '', 1, delay_seconds=20 + i) # > 15
             res[g_wxid]['vp_xw'] = Sys.delayed_task(sky_task_exec, g_wxid, s_wxid, 'vp_xw', delay_seconds=100 + i)  # > 50
             res[g_wxid]['vp_ov_wa'] = Sys.delayed_task(sky_task_exec, g_wxid, s_wxid, 'vp_ov_wa', delay_seconds=130 + i)  # > 30
-            res[g_wxid]['vp_rank'] = Sys.delayed_task(sky_task_exec, g_wxid, s_wxid, 'vp_ov_wa', delay_seconds=160 + i)  # > 30
+            res[g_wxid]['vp_rank'] = Sys.delayed_task(sky_task_exec, g_wxid, s_wxid, 'vp_rank', '#昨日榜', delay_seconds=160 + i)  # > 30
             i += 120
         return self.success(res)
 
--- service/wechat/callback/vp_command_service.py
+++ service/wechat/callback/vp_command_service.py
@@ -415,7 +415,7 @@ class VpCommandService:
         rdb = 'model.wechat.wechat_msg_model.WechatMsgModel.get_msg_times_rank'
         r_list = Transfer.middle_exec(rdb, [], self.g_wxid, m_date_list)
         if r_list:
-            response = f"【{r_list[0]['g_wxid_name']}】#群聊榜单 #{rn} \r\n"
+            response = f"【{r_list[0]['g_wxid_name']}】#群聊榜单 {rn} \r\n"
             for r in r_list:
                 response += f"  - {r['s_wxid_name']}（{r['count']}次)\r\n"
             return self.client.send_msg(response, s_g_wxid, [], self.extra)
