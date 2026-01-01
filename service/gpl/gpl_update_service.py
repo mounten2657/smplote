78864e21f237ac1dc09c690a67fd1ca8f512cffe
smplote <smplote@gmail.com>
2026-01-01 21:21:16 +0800

    增加同步速率

--- service/gpl/gpl_update_service.py
+++ service/gpl/gpl_update_service.py
@@ -156,7 +156,7 @@ class GPLUpdateService:
         l_list['f1'] = {f"{d['h_event']}_{d['h_value']}": d for d in l_list['f1']}
         l_list['f2'] = {f"{d['h_event']}_{d['h_value']}": d for d in l_list['f2']}
 
-        @Ins.multiple_executor(2)
+        @Ins.multiple_executor(4)
         def _up_day_exec(code):
             Time.sleep(Str.randint(1, 10) / 100)
             res = []
