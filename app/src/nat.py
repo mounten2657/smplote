9942f6dcf4c1030561b6712e1d6ecc79911575cc
smplote <smplote@gmail.com>
2026-02-13 10:17:16 +0800

    nat 参数格式化

--- app/src/nat.py
+++ app/src/nat.py
@@ -1,3 +1,4 @@
+from tool.core import Attr
 from tool.router.base_app import BaseApp
 from service.source.nat_service import NatService
 
@@ -12,26 +13,29 @@ class Nat(BaseApp):
         """利用代理池发起请求"""
         method = self.params.get('m', 'GET')
         url = self.params.get('u', '')
-        params = self.params.get('p', '')
+        params = Attr.parse_json_ignore(self.params.get('p', ''))
+        headers = Attr.parse_json_ignore(self.params.get('h', ''))
         if not url:
             return self.error('Invalid url')
-        return self.success(self.nat.ppr_request(method, url, params))
+        return self.success(self.nat.ppr_request(method, url, params, headers))
 
     def vpn(self):
         """利用vpn发起请求"""
         method = self.params.get('m', 'GET')
         url = self.params.get('u', '')
-        params = self.params.get('p', '')
+        params = Attr.parse_json_ignore(self.params.get('p', ''))
+        headers = Attr.parse_json_ignore(self.params.get('h', ''))
         o = self.params.get('o', '0')
         if not url:
             return self.error('Invalid url')
-        return self.success(self.nat.vpn_request(method, url, params, int(o)))
+        return self.success(self.nat.vpn_request(method, url, params, headers, int(o)))
 
     def vps(self):
         """利用vps发起请求"""
         method = self.params.get('m', 'GET')
         url = self.params.get('u', '')
-        params = self.params.get('p', '')
+        headers = Attr.parse_json_ignore(self.params.get('h', ''))
+        params = Attr.parse_json_ignore(self.params.get('p', ''))
         if not url:
             return self.error('Invalid url')
-        return self.success(self.nat.vps_request(method, url, params))
+        return self.success(self.nat.vps_request(method, url, params, headers))
