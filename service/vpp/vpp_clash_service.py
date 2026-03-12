4d38d96d497afca14061118df55c54abdde34f2d
smplote <smplote@gmail.com>
2026-03-12 11:20:37 +0800

    节点统计修复

--- service/vpp/vpp_clash_service.py
+++ service/vpp/vpp_clash_service.py
@@ -232,7 +232,7 @@ class VppClashService:
                 np = next(rv_list)
                 percent = round(np / rv_sum * 100, 2)
                 used = stat['upload'] + stat['download']
-                used = f"{round(used / 1024, 6):.2f}G" if used >= 1024 else f"{used:.2f}M"
+                used = f"{round(used / 1024, 3):.3f}G" if used >= 1024 else f"{used:.2f}M"
                 stat = f"{used}/{stat['total']}G,{stat['expire'] if stat['expire'] else 9999}天,{np}/{percent:.2f}%"
             md += f" - {sub}: {stat}\r\n"
         return md
