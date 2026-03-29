419be52f08e5051142e958551f9100dc6a1a854f
smplote <smplote@gmail.com>
2026-03-29 18:26:32 +0800

    保留错误日志

--- wsgi.conf.py
+++ wsgi.conf.py
@@ -71,9 +71,9 @@ limit_request_field_size = 8190  # 单个请求头最大字节
 # 日志配置
 # ---------------------------
 # 访问日志 (None表示不记录，'-' 代表标准输出)
-accesslog = f'-'
+accesslog = None
 # 错误日志 (必填，'-' 代表标准输出)
-errorlog = f'-'
+errorlog = '-'
 # 日志级别 (debug/info/warning/error/critical)
 loglevel = 'debug'
 # 日志格式
